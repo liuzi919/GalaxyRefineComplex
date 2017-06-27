9e82941f497c95f01275a03df5a11af08b3c672a
seoklab <compbio.galaxy@gmail.com>
2017-06-27 12:19:59 +0900

    new example files

---
+++ example/hetero_complex.sh
@@ -0,0 +1,2 @@
+#export GALAXY_HOME=PATH_TO_THE_GALAXYREFINECOMPLEX
+../bin/GalaxyRefineComplex -t hetero_complex -p hetero_complex.pdb
---
+++ example/homo_oligomer.sh
@@ -0,0 +1,2 @@
+#export GALAXY_HOME=PATH_TO_THE_GALAXYREFINECOMPLEX
+../bin/GalaxyRefineComplex -t homo_oligomer -p homo_oligomer.pdb --sym
